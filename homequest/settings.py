c2f161da5b8c6dcce4f7c0de487324c633192b89
danvm94 <danvm94@gmail.com>
2023-10-14 17:57:29 +0000

    Add the Heroku link to the ALLOWED_HOSTS

--- homequest/settings.py
+++ homequest/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == 'True' else False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1',
+                 'https://homequestrs-494f18176619.herokuapp.com/']
 
 # Application definition
 
