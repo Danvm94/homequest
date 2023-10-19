a7ad37063f05cd6e6c9a14cbfceb23af7f5ed9aa
danvm94 <danvm94@gmail.com>
2023-10-19 07:59:05 +0000

    Adjust login and register view for a built in Django message with Bootstrap toast instead a manual JS Ajax

--- user_management/views.py
+++ user_management/views.py
@@ -1,20 +1,40 @@
 from allauth.account.views import LoginView, SignupView
 from django.http import JsonResponse
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 
 class CustomLoginView(LoginView):
     def form_invalid(self, form):
-        # Check if the request is an AJAX request
-        if self.request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
-            return JsonResponse({'form': form.errors}, status=400)
-        return super(CustomLoginView, self).form_invalid(form)
+        form_errors = form.errors
+        error_messages = []
+
+        # Iterate through the form errors and collect all error messages
+        for field, errors in form_errors.items():
+            for error in errors:
+                error_messages.append(error)
+
+        # Add all error messages to the messages framework
+        for error_message in error_messages:
+            messages.add_message(self.request, messages.ERROR, error_message)
+
+        # Redirect to the referer page
+        return HttpResponseRedirect(self.request.META.get('HTTP_REFERER'))
 
 
 class CustomSignupView(SignupView):
     def form_invalid(self, form):
-        # Check if the request is an AJAX request
-        if self.request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
-            print(form.errors)
-            print({'form': form.errors})
-            return JsonResponse({'form': form.errors}, status=400)
-        return super(CustomSignupView, self).form_invalid(form)
+        form_errors = form.errors
+        error_messages = []
+
+        # Iterate through the form errors and collect all error messages
+        for field, errors in form_errors.items():
+            for error in errors:
+                error_messages.append(error)
+
+        # Add all error messages to the messages framework
+        for error_message in error_messages:
+            messages.add_message(self.request, messages.ERROR, error_message)
+
+        # Redirect to the referer page
+        return HttpResponseRedirect(self.request.META.get('HTTP_REFERER'))
