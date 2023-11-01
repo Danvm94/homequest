4646dd535b91ecbb5a0a8082649e2945011a55f0
danvm94@gmail.com <danvm94@gmail.com>
2023-11-01 08:06:39 +0000

    Refactor index template for a better visual

--- homepage/views.py
+++ homepage/views.py
@@ -5,8 +5,9 @@ from properties.utils import get_latest_properties
 # Create your views here.
 def index_view(request):
     latest_properties = get_latest_properties(8)
+
     context = {
-        'latest_properties': latest_properties
+        'latest_properties': latest_properties,
     }
     return render(request, 'index.html', context)
 
