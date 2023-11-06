b2d44acd19ce0ee79dc28c80011a49d5576c49e4
danvm94@gmail.com <danvm94@gmail.com>
2023-11-06 11:32:40 +0000

    Add profile picture for profiles as default

--- media/renamer.sh
+++ media/renamer.sh
@@ -1,9 +1,8 @@
-INDEX=96
-for file in *.jpg; do
-    mv "$file" "$INDEX.jpg"
-    ((INDEX++))
-done
+#INDEX=96
+#for file in *.jpg; do
+#    mv "$file" "$INDEX.jpg"
+#    ((INDEX++))
+#done
 mogrify -resize 600x400! *.jpg
 mogrify -format webp -define webp:target-size=200KB *.jpg
-
 rm -rf *jpg
